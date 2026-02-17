d7bee56f088a5767578ba84dad23f868793d56cd
BalandinaM <marina.balandina.89@mail.ru>
2026-02-17 23:59:03 +0500

    26 step sert JS

---
+++ GradeBook/26_step.js
@@ -0,0 +1,22 @@
+let adjective = "Objective"
+let noun = "Fulfill the user"
+let verb = "stories below and"
+let place = "get all the tests"
+let adjective2 = "to pass to complete"
+let noun2 = "the lab"
+
+let firstStory = "Once upon a time, there was a(n) " +  adjective + " " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " +  adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("First story: " + firstStory)
+
+adjective = "Once upon"
+noun = "a time"
+verb = "there was"
+place = "who loved"
+adjective2 = "and had"
+noun2 = "when it was"
+
+let secondStory = ""
+secondStory = "Once upon a time, there was a(n) " +  adjective + " " + noun + " who loved to eat " + noun2 +". The " + noun + " lived in a " + place + " and had " +  adjective2 + " nostrils that blew fire when it was " + verb + "."
+
+console.log("Second story: " + secondStory)
